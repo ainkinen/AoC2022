8b9fef78ec13b87a7b2ab3f3634813888a5f75c7
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2023-12-17 00:16:57 +0200

    Add template day

---
+++ src/solutions/dayX/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from src.utils.files import read_as_lines
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def part1(input_path: str = INPUT_PATH) -> int:
+    read_as_lines(input_path)
+    return 0
+
+
+def part2(input_path: str = INPUT_PATH) -> int:
+    read_as_lines(input_path)
+    return 0
